69632096c1083f430852e4544202befc0ecf3d1f
sayaotonathi <sayaotonathi@gmail.com>
2021-04-26 11:26:44 +0800

    First upload

---
+++ MineSweaper/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweaper
+{
+    public partial class Form1 : Form
+    {
+        int xCount = 9;
+        int yCount = 9;
+        Button[,] btn ;
+        GameClass theGame = GameClass.Instance();
+        int[,] mineMap = new int[9, 9];
+
+        public Form1()
+        {
+            InitializeComponent();
+            generateButtons();
+        }
+
+        
+
+        private void generateButtons()
+        {
+            btn = new Button[xCount, yCount];
+            this.SuspendLayout();
+            for (int y = 0; y < yCount; y++)
+            {
+                for (int x = 0; x < xCount; x++)
+                {
+                    btn[x, y] = new Button();
+                    btn[x, y].Size = new Size(30, 30);
+                    btn[x, y].Location = new Point(10 + 31 * x, 10 + 31 * y);
+                    btn[x, y].Name = ((x + 1) * (y + 1)).ToString();
+                    btn[x, y].Click += new System.EventHandler(btn_Click);
+                    btn[x, y].EnabledChanged += new System.EventHandler(this.btn_EnabledChanged);
+                    this.splitContainer1.Panel2.Controls.Add(btn[x, y]);
+                    theGame.setMine(mineMap);
+                    //theGame.scanMine(xCount, yCount, mineMap);
+                }
+            }
+            this.ResumeLayout();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Enabled = false;
+            //MessageBox.Show(btn.Name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            theGame.setMine(mineMap);
+            string s = "";
+            theGame.scanMine(xCount, yCount, mineMap);
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    s += mineMap[i, j].ToString().PadLeft(2,' ' ) + " ";
+                }
+                s += "\n";
+            }
+            Console.WriteLine(s);
+        }
+
+        private void btn_EnabledChanged(object sender, EventArgs e)
+        {
+            theGame.autoFlip(xCount, yCount, mineMap, btn);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    s += mineMap[i, j].ToString().PadLeft(2, ' ') + " ";
+                }
+                s += "\n";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ MineSweaper/GameClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweaper
+{
+    class GameClass
+    {
+        private readonly static GameClass _instance = new GameClass();
+
+        private GameClass()
+        {
+        }
+
+        public static GameClass Instance()
+        {
+            return _instance;
+        }
+
+        public  delegate void delegateScanNear();
+
+
+
+        public void scanRange(int x,int y ,int X, int Y,out int minx,out int maxx,out int miny,out int maxy)
+        {
+            minx = (x == 0) ? 0 : x - 1;
+            maxx = (x == X - 1) ? X - 1 : x + 1;
+            miny = (y == 0) ? 0 : y - 1;
+            maxy = (y == Y - 1) ? Y - 1 : y + 1;
+        }
+
+
+
+        public  void scanMine(int X, int Y,int[,] a)
+        {
+            for (int y = 0; y < Y; y++)
+            {
+                for (int x = 0; x < X; x++)
+                {
+                    int minx = (x == 0) ? 0 : x - 1;
+                    int maxx = (x == X - 1) ? X - 1 : x + 1;
+                    int miny = (y == 0) ? 0 : y - 1;
+                    int maxy = (y == Y - 1) ? Y - 1 : y + 1;
+                    if (a[x,y] <0)
+                    {
+                        for (int j = miny; j <= maxy; j++)
+                        {
+                            for (int i = minx; i <= maxx; i++)
+                            {
+                                a[i, j] += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void setMine(int[,] a)
+        {
+            a[2, 6] = -9;
+            a[4, 6] = -9;
+            a[0, 8] = -9;
+            a[8, 0] = -9;
+            a[8, 8] = -9;
+        }
+
+        public void autoFlip(int X,int Y, int[,] a,Button[,]b)
+        {
+            for (int y = 0; y < Y; y++)
+            {
+                for (int x = 0; x < X; x++)
+                {
+                    int minx = (x == 0) ? 0 : x - 1;
+                    int maxx = (x == X - 1) ? X - 1 : x + 1;
+                    int miny = (y == 0) ? 0 : y - 1;
+                    int maxy = (y == Y - 1) ? Y - 1 : y + 1;
+                    if (a[x, y] == 0)
+                    {
+                        for (int j = miny; j <= maxy; j++)
+                        {
+                            for (int i = minx; i <= maxx; i++)
+                            {
+                                b[i, j].Enabled = false ;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
